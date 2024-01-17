3fa49b8084c938b7f1ae2ebed492bc19a54aac09
Zenalvo Junior <juniorrosalvo2015@gmail.com>
2024-01-17 00:44:12 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -71,8 +71,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken] // Annotation to prevent my application from being attacked by CSRF.
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
